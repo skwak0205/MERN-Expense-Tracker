e99d62918432b36b0d21a0f7822dacf5cf3f48fd
Soojung Kwak <gsj2464@gmail.com>
2020-06-26 20:57:48 -0400

    modified

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const config = require("./key");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(config.mongoURI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,

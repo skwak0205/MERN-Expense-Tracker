e08dc1e0b9e10c84b4faa79fc1fa9f66da37d73a
Soojung Kwak <gsj2464@gmail.com>
2020-06-26 20:10:58 -0400

    initial commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const config = require("./key");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(config.mongoURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (error) {
+    console.log(`Error: ${error.message}`.red);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
